4525616be175f0107326c033fa25c5ff605e086b
sayedali1 <said.ali199859@gmail.com>
2022-12-08 21:18:28 +0000

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n
-	prin	Size of an int: %d byte(s)\n
+		Size of an int: %d byte(s)\n
 		Size of a long int: %d byte(s)\n
 		Size of a long long int: %d byte(s)\n
 		Size of a float: %dbyte(s)", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int));
