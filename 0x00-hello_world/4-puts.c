da49d38ad907fcc0aa878b7ccf7020ab89c98e6e
sayedali1 <said.ali199859@gmail.com>
2022-12-08 20:48:19 +0000

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
-/*print a massage in the standerout*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+
 	int puts('"Programming is like building a multilingual puzzle\n');
 	return (0);
 }
