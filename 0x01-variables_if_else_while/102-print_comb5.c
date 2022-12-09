60cb43aa20b7db77e3a7e002ebf94e8ed6890d05
sayedali1 <said.ali199859@gmail.com>
2022-12-09 20:35:53 +0000

    task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0
+*/
+int main(void)
+{
+	int num1 = 48;
+	int num2 = 48;
+	int num3 = 48;
+	int num4 = 49;
+	int comma = 44;
+	int space = 32;
+	int newLine = 10;
+
+	while (num1 <= 57)
+	{
+		while (num2 <= 57)
+		{
+			while (num3 <= 57)
+			{
+				while (num4 <= 57)
+				{
+					if (num1 > num2)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(space);
+						putchar(num3);
+						putchar(num4);
+					}
+
+					if (num1 < 58 && num2 < 57)
+					{
+					putchar(comma);
+					putchar(space);
+					}
+					num4++;
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar(newLine);
+	return (0);
+}
