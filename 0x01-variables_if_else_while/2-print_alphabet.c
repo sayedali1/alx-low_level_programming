158939bfe3c9426c204905ea27318d3b8973b4e3
sayedali1 <said.ali199859@gmail.com>
2022-12-09 16:49:23 +0000

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: always 0
+*/
+
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha < 123)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	return(0)
+}
