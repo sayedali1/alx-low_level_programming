3e1b03b16c3ec0d4ccc5c3f0fd835a6861501cbc
sayedali1 <said.ali199859@gmail.com>
2022-12-09 23:51:13 +0000

    task102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,9 +17,9 @@ int main(void)
 	{
 		for (num2 = 48; num2 <= 57; num2++)
 		{
-			for (num3 = num1; num3 <= 57; num3++)
+			for (num3 = 48; num3 <= 57; num3++)
 			{
-				for (num4 = num2 + 1; num4 <= 57; num4++)
+				for (num4 = num3 + 1; num4 <= 57; num4++)
 				{
 					putchar(num1);
 					putchar(num2);
