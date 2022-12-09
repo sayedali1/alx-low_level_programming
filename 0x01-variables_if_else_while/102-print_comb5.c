5460d83b1852f9aa94bcd0a2df3b59f16f83745b
sayedali1 <said.ali199859@gmail.com>
2022-12-09 21:33:25 +0000

    task102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 			{
 				while (num4 <= 57)
 				{
-					if (num1 <= num4)
+					if (num1 <= num3)
 					{
 						putchar(num1);
 						putchar(num2);
@@ -29,7 +29,7 @@ int main(void)
 						putchar(num3);
 						putchar(num4);
 					}
-					if (num1 <= num4 && num1 < 58)
+					if (num1 <= num3 && num1 < 58)
 					{
 						putchar(comma);
 						putchar(space);
