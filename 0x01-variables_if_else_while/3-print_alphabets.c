303cc46975696f7dd3c579ac8a56a27b85c8f98e
sayedali1 <said.ali199859@gmail.com>
2022-12-09 17:07:34 +0000

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: always 0
+*/
+
+int main(void)
+{
+	int low_alpha = 97;
+
+	while (low_alpha < 123)
+	{
+		putchar(low_alpha);
+		alpha++;
+	}
+
+	int upper_alpha = 65;
+
+	while (upper_alpha < 91)
+	{
+		putchar(upper_aplha);
+		upper_alpha++;
+	}
+	putchar(10);
+	return (0);
+}
