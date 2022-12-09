e031cf36356a7c3b700efc15b13bf1fd3481c4a2
sayedali1 <said.ali199859@gmail.com>
2022-12-09 17:42:41 +0000

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: always 0
+*/
+
+int main(void)
+{
+	int alpha = 122;
+
+	while (alpha >= 95)
+	{
+		putchar(alpha);
+		alpha--;
+	}
+
+	putchar(10);
+
+	return (0);
+}
