f7ae530660870760d9719c94b18560a9f7a341ef
sayedali1 <said.ali199859@gmail.com>
2022-12-09 17:12:56 +0000

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,12 @@
 int main(void)
 {
 	int low_alpha = 97;
-
+	int upper_alpha = 65;
 	while (low_alpha < 123)
 	{
 		putchar(low_alpha);
 		low_alpha++;
 	}
-	
-	int upper_alpha = 65;
 
 	while (upper_alpha < 91)
 	{
