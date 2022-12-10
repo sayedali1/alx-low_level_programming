c37f7524b7bd562f725181e85e5eee2c80244c81
sayedali1 <said.ali199859@gmail.com>
2022-12-10 01:30:24 +0000

    task102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,39 +7,24 @@ int main(void)
 {
 	int num_1;
 	int num_2;
-	int num_3;
-	int num_4;
 	int comma = 44;
 	int space = 32;
 	int newLine = 10;
 
-	for (num_1 = 48; num_1 <= 57; num_1++)
+	for (num_1 = 0; num_1 <= 98; num_1++)
 	{
-		for (num_2 = 48; num_2 <= 57; num_2++)
+		for (num_2 = num_1 + 1; num_2 <= 99; num_2++)
 		{
-			for (num_3 = 48; num_3 <= 57; num_3++)
-			{
-				for (num_4 = 48 ; num_4 <= 57; num_4++)
-				{
-					if(((num_3 + num_4) > (num_1 + num_2) &&  num_3 >= num_1 ) || num_1 < num_3)
-					{
-						putchar(num_1);
-						putchar(num_2);
-						putchar(space);
-						putchar(num_3);
-						putchar(num_4);
-					}
+			putchar((num_1 / 10) + '0');
+			putchar((num_1 % 10) + '0');
+			putchar(space);
+			putchar((num_2 / 10) + '0');
+			putchar((num_2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
 
-					if (!((num_1 == 57 && num_2 == 56) && (num_3 == 57 && num_4 == 57)))
-					{
-						if(((num_3 + num_4) > (num_1 + num_2) &&  num_3 >= num_1 ) || num_1 < num_3)
-						{
-							putchar(comma);
-							putchar(space);
-						}
-					}
-				}
-			}
+			putchar(comma);
+			putchar(space);
 		}
 	}
 	putchar(newLine);
