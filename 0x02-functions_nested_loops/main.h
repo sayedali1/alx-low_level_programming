f871296a6aa676db21b79d1b7d1086c5b6e36210
sayedali1 <said.ali199859@gmail.com>
2022-12-13 20:04:26 +0000

    define

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -7,26 +10,60 @@
  */
 int _putchar(char c);
 
-
-
+/**
+*print_alphabet - print the alpha
+*
+*Return: always 0
+*/
 void print_alphabet(void);
 
-
+/**
+*print_alphabet_x10 - print alphabet 10 times
+*return: void
+*/
 
 void print_alphabet_x10(void);
 
-
+/**
+*_islower - entry point check if c lower alpha or not
+*
+*@c: integar
+*
+*Return:  0 if not_lower, 1 if lower
+*/
 int _islower(int c);
 
-
+/**
+*_isalpha - entry point check if c  alpha or not
+*
+*@c: integar
+*
+*Return:   1 if alphabet, 0 otherwise
+*/
 int _isalpha(int c);
 
-
+/**
+*print_sign - check the sign og a number
+*
+*@n: int num
+*
+*Return: 1 if postive, 0 if zero, -1 if negative
+*/
 int print_sign(int n);
 
-
+/**
+*_abs - get the absolute value
+*
+*@int: int to return
+*Return: absolute value
+*/
 int _abs(int);
 
-
-
+/**
+*print_last_digit - get the last diger of num
+*@int: int to return
+*Return: the absolute of the last digit
+*/
 int print_last_digit(int);
+
+#endif
