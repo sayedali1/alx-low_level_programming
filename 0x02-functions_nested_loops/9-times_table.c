4577c04739049b3d586d8fd720cbaddbd7faec54
sayedali1 <said.ali199859@gmail.com>
2022-12-13 21:29:20 +0000

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,16 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			_putchar('0' + result / 10);
-			_putchar('0' + result % 10);
+
+			if (result < 9)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
 			_putchar(',');
 			_putchar(' ');
 		}
