5e1906926040648b8e84954138811780cdf842dd
sayedali1 <said.ali199859@gmail.com>
2022-12-13 18:55:38 +0000

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 /**
 *print_sign - check the sign og a number
 *
-*@n int num 
+*@n: int num
 *
-*Return 1 if postive, 0 if zero, -1 if negative
+*Return: 1 if postive, 0 if zero, -1 if negative
 */
 int print_sign(int n)
 {
