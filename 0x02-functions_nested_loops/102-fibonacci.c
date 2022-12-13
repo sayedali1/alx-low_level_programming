9bdc81abea50ba16237075ceebf4b6943a201164
sayedali1 <said.ali199859@gmail.com>
2022-12-13 23:54:43 +0000

    task102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - fibonacci serise
+*Return: print the fib
+*/
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+	int sum = 0;
+	int i;
+
+	printf("%d, %d", num1, num2);
+	for (i = 0; i < 49; i++)
+	{
+		sum = num1 + num2;
+		printf("%d, ", sum);
+		num1 = num2;
+		num2 = sum;
+	}
+	printf("\n");
+	return (0);
+}
