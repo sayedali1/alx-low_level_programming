d24b0ab1840524f0399da04c2f912ebeada2291e
sayedali1 <said.ali199859@gmail.com>
2022-12-13 23:18:26 +0000

    task100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,36 +14,38 @@ void print_times_table(int n)
 
 	if (n > 15 || n < 0)
 	{
-		return (0);
-	}
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
-
-			if (j == 0)
-			{
-				printf("%d", result);
-			}
-			else if (result <= 9)
-			{
-				printf("%d,   ", result);
-			}
-			else if (result > 99)
-			{
-				printf("%d,  ", result);
-			}
-			else if (result > 999)
-			{
-				printf("%d, ", result);
-			}
-			if (j == 9)
-			{
-				continue;
-			}
-		}
 		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+
+				if (j == 0)
+				{
+					printf("%d", result);
+				}
+				else if (result <= 9)
+				{
+					printf("%d,   ", result);
+				}
+				else if (result > 99)
+				{
+					printf("%d,  ", result);
+				}
+				else if (result > 999)
+				{
+					printf("%d, ", result);
+				}
+				if (j == 9)
+				{
+					continue;
+				}
+			}
+			printf("\n");
+		}
 	}
 }
