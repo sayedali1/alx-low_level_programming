f9764699988cc784f6c004c5837aa512095436cd
sayedali1 <said.ali199859@gmail.com>
2022-12-13 22:20:26 +0000

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add - add two numbers
+*@a: first int number
+*@b: sec int number
+*Return: the result of the sum
+*/
+int add(int a, int b)
+{
+	int result = 0;
+
+	result = a + b;
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -79,4 +79,12 @@ void jack_bauer(void);
 */
 void times_table(void);
 
+/**
+*add - add two numbers
+*@int: first int number
+*@int: sec int number
+*Return: the result of the sum
+*/
+int add(int, int);
+
 #endif
