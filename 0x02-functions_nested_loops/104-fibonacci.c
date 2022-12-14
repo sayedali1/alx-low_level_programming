57227494959b83f028c9f3f7b1b82f1f778518f4
sayedali1 <said.ali199859@gmail.com>
2022-12-14 00:26:40 +0000

    task104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - fibonacci serise
+*Return: print the fib
+*/
+int main(void)
+{
+	long num1 = 1;
+	long num2 = 2;
+	long sum = 0;
+	int i;
+
+	printf("%ld, %ld", num1, num2);
+	for (i = 0; i < 48; i++)
+	{
+		sum = num1 + num2;
+		printf(", %ld", sum);
+		num1 = num2;
+		num2 = sum;
+	}
+	printf("\n");
+	return (0);
+}
