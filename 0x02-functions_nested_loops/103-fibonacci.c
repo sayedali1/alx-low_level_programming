f253ff54d8f9af6ab3ccfa9543e6091be8707356
sayedali1 <said.ali199859@gmail.com>
2022-12-14 00:20:17 +0000

    task103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - fibonacci serise
+*Return: print sum of even num in the fib
+*/
+int main(void)
+{
+	long num1 = 1;
+	long num2 = 2;
+	long sum = 0;
+	int i;
+	long even_sum = 2;
+
+	for (i = 0; i < 4000000; i++)
+	{
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+
+		if (sum % 2 == 0)
+		{
+			even_sum += sum;
+		}
+	}
+	printf("%ld", even_sum);
+	printf("\n");
+	return (0);
+}
