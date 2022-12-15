b7817dc56e5d09621868be21ee9e46006c6025d7
sayedali1 <said.ali199859@gmail.com>
2022-12-15 23:32:50 +0000

    task100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,8 +6,8 @@
 
 int main(void)
 {
-	long i, prime;
-	long n = 612852475143;
+	long int i, prime;
+	long int n = 612852475143;
 
 	for (i = 2; i < n; i++)
 	{
@@ -19,6 +19,6 @@ int main(void)
 		}
 	}
 
-	printf("%ld\n", prime);
+	printf("%lu\n", prime);
 	return (0);
 }
