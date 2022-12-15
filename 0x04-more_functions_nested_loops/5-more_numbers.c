e922dc40690334e94635bb7fd79886fee0ec0851
sayedali1 <said.ali199859@gmail.com>
2022-12-15 09:54:04 +0000

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,8 +11,14 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + j / 10);
-			_putchar('0' + j % 10);
+			if (j <= 14)
+			{
+				_putchar('0' + j / 10);
+			}
+			if (j > 9 && j <= 14)
+			{
+				_putchar('0' + j % 10);
+			}
 		}
 
 		_putchar('\n');
