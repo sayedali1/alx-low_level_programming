749a1f4b8aadf517d7afd26380839160e769f908
sayedali1 <said.ali199859@gmail.com>
2022-12-15 12:03:12 +0000

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_square - print # for size times
+*@size: num of the # to be printed
+*Return: void
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
