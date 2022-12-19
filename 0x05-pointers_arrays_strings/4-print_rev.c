e6e35ca09400fe10df60205df4d9e3f6c5c3636a
saidali1 <said.ali199859@gmail.com>
2022-12-19 06:36:37 -0800

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,15 @@
 void print_rev(char *s)
 {
 	int len = 0;
+	int i;
 
 	while (*s != '\0')
 	{
-		len++
+		len++;
 		s++;
 	}
 
-	for (i = len;i > 0, i--)
+	for (i = len; i > 0; i--)
 	{
 		_putchar(*(s + i));
 	}
