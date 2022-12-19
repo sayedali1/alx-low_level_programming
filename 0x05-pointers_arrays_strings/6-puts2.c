fcf9d9bebafb6af9d86cc603bfb20c07d2f32998
saidali1 <said.ali199859@gmail.com>
2022-12-19 08:49:53 -0800

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*puts2 - print char
+*@str: address og the str
+*Return: void
+*/
+void puts2(char *str)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s += 2;
+	}
+
+}
+
