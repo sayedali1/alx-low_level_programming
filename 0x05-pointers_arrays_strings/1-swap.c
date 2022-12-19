7243e0fed05e9f93f18fb564f9b46fe7ad84151a
saidali1 <said.ali199859@gmail.com>
2022-12-19 04:55:13 -0800

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*swap_int - swap two int
+*@a: first int 
+*@b: sec int
+*Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
