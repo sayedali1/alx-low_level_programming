f420e9dea7c63a495b3ebd5f845b8817e10b0f60
saidali1 <said.ali199859@gmail.com>
2022-12-19 07:17:28 -0800

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+inlcude "main.h"
+/**
+*rev_string -  function that reverses a string.
+*@s: the first address of the str
+*Return void
+*/
+
+void rev_string(char *s);
+{
+	int len = 0;
+	int i;
+	char *p;
+
+	while(*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	s--;
+	for (i = len; i > 0 ;i--)
+	{
+		p = s;
+		p++;
+		s--;
+	}
+	for (i = len; i > 0; i-- )
+	{
+		s = p;
+		s++;
+		p--;
+	}
+}
+
