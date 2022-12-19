2ce7745d66d982ac34eed5529fc59953909538be
saidali1 <said.ali199859@gmail.com>
2022-12-19 09:14:34 -0800

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,42 @@
 #include "main.h"
 
 /**
-*puts2 - print char
-*@str: address og the str
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+
+
+
+int _strlen(char *s)
+
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/*
+*puts2 - prints every other character of a string
+*@str: the address of the first str
 *Return: void
 */
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str += 2;
-	}
+	int i = 0;
+	int len = _strlen(s);
 
+	while (s[i] != '\0' && i < len)
+	{
+		_putchar(s[i]);
+		i += 2;
+
+	}
+	_putchar('\n');
 }
 
