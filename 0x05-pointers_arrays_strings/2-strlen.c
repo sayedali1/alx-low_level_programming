112a2dbaccee3b6b489bb68a80c76287c019d22b
saidali1 <said.ali199859@gmail.com>
2022-12-19 05:09:13 -0800

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - get the len of the string
+* @s: the string
+* Return: the len
+*/
+int _strlen(char *s)
+{
+	int lenth;
+
+	lenth = sizeof(s) / sizeof(int);
+
+	return (lenth);
+}
+
