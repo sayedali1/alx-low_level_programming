8ba3a5cc5c66a2613c268bac009b53bd25f8d570
saidali1 <said.ali199859@gmail.com>
2022-12-19 07:50:56 -0800

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *rev_string -  function that reverses a string.
 *@s: the first address of the str
@@ -8,29 +7,23 @@
 void rev_string(char *s)
 {
 	int len = 0;
-	char *p;
-	int i=0;
+	char temp;
+	int i, j = 0;
 
 	while(*s != '\0')
 	{
 		len++;
 		s++;
 	}
-
 	s--;
+
 	for (i = len; i > 0 ;i--)
 	{
-		p = s;
-		p++;
-		s--;
+		temp = s[j];
+		s[j] = s[len];
+		s[len] = temp;
+		j++;	
 
-	}
-	printf("%s\n",p);
-	for (i = len; i > 0; i-- )
-	{
-		s = p;
-		s++;
-		p--;
 	}
 }
 
