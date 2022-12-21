e761b0f57fcb0070b486a142089a787b313db50c
sayed <said.ali199859@gmail.com>
2022-12-21 20:51:11 +0200

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+int _strcmp(char *s1, char *s2)
+{
+    int i;
+
+    for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+    {
+        if (s1[i] == s2[i])
+        {
+            return (0);
+        }
+        else if (s1[i] < s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
+        else
+        {
+            return (s1[i] - s2[i]);
+        }
+    }
+}
