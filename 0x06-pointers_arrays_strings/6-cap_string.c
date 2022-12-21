e91b0302da7b402abb7debc8d99917541da2ee9f
saidali1 <said.ali199859@gmail.com>
2022-12-21 14:55:05 -0800

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *cap_string - capitalizes all words of a string.
 *@cap: str that we need to cap
@@ -6,19 +7,21 @@
 */
 char *cap_string(char *cap)
 {
-    char separator[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-    int i, j;
+	char separator[] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+	int i, j;
 
-    for (i = 0; cap[i] != '\0'; i++)
-    {
-        for (j = 0; separator[j] != '\0'; j++)
-        {
-            if (cap[i] == separator[j] && cap[i + 1] <= 'z' && cap[i + 1] >= 'a')
-            {
-                cap[i + 1] -= 32;
-            }
-        }
-    }
+	for (i = 0; cap[i] != '\0'; i++)
+	{
+		for (j = 0; separator[j] != '\0'; j++)
+		{
+			if (cap[i] == separator[j] && cap[i + 1] <= 'z' && cap[i + 1] >= 'a')
+			{
+				cap[i + 1] -= 32;
+			}
+		}
+	}
 
-    return (cap);
+	return (cap);
 }
+
