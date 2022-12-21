20d7ce7724fc89794dcb6fb3434c5f244066ffa6
saidali1 <said.ali199859@gmail.com>
2022-12-21 10:56:14 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,30 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * _strcmp - compare two str
+ * @s1: fir str
+ * @s2:sec str
+ * Return: 0 if equal, postive if greater, negative if less
+ */
+
 int _strcmp(char *s1, char *s2)
 {
-    int i;
+	int i;
 
-    for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-    {
-        if (s1[i] == s2[i])
-        {
-            return (0);
-        }
-        else if (s1[i] < s2[i])
-        {
-            return (s1[i] - s2[i]);
-        }
-        else
-        {
-            return (s1[i] - s2[i]);
-        }
-    }
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			return (0);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
 }
+
