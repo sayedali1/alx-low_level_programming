3203dfc16a423a93c11ac9c3c20bb8cda29fac9e
saidali1 <said.ali199859@gmail.com>
2022-12-21 09:44:08 -0800

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,31 +6,34 @@
  */
 int _strlen(char *s)
 {
-    int lenth = 0;
+	int lenth = 0;
 
-    while (*s != '\0')
-    {
-        lenth++;
-        s++;
-    }
+	while (*s != '\0')
+	{
+		lenth++;
+		s++;
+	}
 
-    return (lenth);
+	return (lenth);
 }
+
 /**
- *_strncar - cat n char
- *@dest: fri str
- *@src: sec str
- *@n:num of char to be concat
- */
+* _strncat - cat n char
+*@dest: fri str
+*@src: sec str
+*@n:num of char to be concat
+*Return: dest
+*/
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, len = _strlen(dest);
+	int i, len = _strlen(dest);
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[len + i] = src[i];
-    }
-    dest[len + i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
 
-    return (dest);
+	return (dest);
 }
+
