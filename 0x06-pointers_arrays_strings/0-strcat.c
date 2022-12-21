2bfd71d2acfd23fa665398d7b4d34f1e53e22563
sayed <said.ali199859@gmail.com>
2022-12-21 19:07:38 +0200

    task2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _strlen - get the len of the string
-* @s: the string
-* Return: the len
-*/
+ * _strlen - get the len of the string
+ * @s: the string
+ * Return: the len
+ */
 int _strlen(char *s)
 {
-	int len = 0;
+    int len = 0;
 
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
+    while (*s != '\0')
+    {
+        len++;
+        s++;
+    }
 
-	return (len);
+    return (len);
 }
 
 /**
-*_strcat - concat to str
-*@dest: first str
-*@src: sec str
-*Return: dest + src
-*/
+ *_strcat - concat to str
+ *@dest: first str
+ *@src: sec str
+ *Return: dest + src
+ */
 char *_strcat(char *dest, char *src)
 {
     int i, len = _strlen(dest);
@@ -33,7 +33,6 @@ char *_strcat(char *dest, char *src)
     for (i = 0; src[i] != '\0'; i++)
     {
         dest[len + i] = src[i];
-        
     }
 
     dest[len + i] = '\0';
