6f6b86908ff0dfaf5e485527f0245f98af0f7c09
sayed <said.ali199859@gmail.com>
2022-12-21 15:05:59 +0200

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,38 @@
-char *_strcat(char *dest, char *src);
+#include "main.h"
+
+/**
+* _strlen - get the len of the string
+* @s: the string
+* Return: the len
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+*_strcat - concat to str
+*@dest: first str
+*@src: sec str
+*Return: dest + src
+*/
+char *_strcat(char *dest, char *src)
+{
+    int i, len = _strlen(dest);
+
+    for (i = 0; src[i] == '\0'; i++)
+    {
+        dest[len] = src[i];
+    }
+    dest[i] = '\0';
+
+    return(dest);
+}
