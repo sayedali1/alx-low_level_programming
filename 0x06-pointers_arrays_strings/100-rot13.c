7c3330f831c2b5af97e38de0a475ec97a5eb0595
saidali1 <said.ali199859@gmail.com>
2022-12-22 04:13:41 -0800

    task100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,34 +1,37 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+* rot13 - encodes a string using rot13.
+* @str: str to be encoded
+* Return: str
+*/
 char *rot13(char *str)
 {
-    int i, j;
-    char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-                'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                'W', 'X', 'Y', 'Z'};
+	int i, j;
+	char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
+	char b[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M'};
 
-    char b[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-                'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                'J', 'K', 'L', 'M'};
+	for (i = 0; str[i] != '\0'; i++)
+	{
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
+		for (j = 0; (a[j] != '\0' && str[i] != a[j]);)
+		{
+			j++;
+		}
 
-        for (j = 0; (a[j] != '\0' && str[i] != a[j]);)
-        {
-            j++;
-        }
-
-        if (str[i] == a[j])
-        {
-            str[i] = b[j];
-
-            printf("%s\n", str);
-        }
-    }
-    return (str);
+		if (str[i] == a[j])
+		{
+			str[i] = b[j];
+		}
+	}
+	return (str);
 }
+
