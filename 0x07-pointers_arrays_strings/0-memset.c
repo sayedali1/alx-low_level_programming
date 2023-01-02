4ca687482f09c7c55693e53c8923a6cfc05a5b84
saidali1 <said.ali199859@gmail.com>
2023-01-02 04:48:38 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,19 @@
 /**
-*_memset - function fills the first n bytes 
-*@s: memory area 
+*_memset - function fills the first n bytes
+*@s: memory area
 *@b: constant
-*@n: size of array 
+*@n: size of array
 *Return: pointer to s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;	
-	for (i = 0; i <= n; i ++)
+	int i;
+
+	for (i = 0; i <= n; i++)
 	{
-		s[i] = b; 
+		s[i] = b;
 	}
 
-	return (s); 
+	return (s);
 }
+
