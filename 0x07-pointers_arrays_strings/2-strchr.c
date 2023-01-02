47c306e31378c27a1082bab413615c024f981ebd
sayed <said.ali199859@gmail.com>
2023-01-02 16:42:31 +0200

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
+		i++;
 	}
-	return (NULL);
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
 }
