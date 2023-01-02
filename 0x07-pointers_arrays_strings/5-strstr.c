f291fffe1729655927f3050f8cabc9a26abf5fea
sayed <said.ali199859@gmail.com>
2023-01-02 20:31:35 +0200

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,14 +11,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, x = 0;
 
+	if (*needle == '\0')
+	{
+		return (0);
+	}
 	i = 0;
-
 	while (*(haystack + i) != '\0')
 	{
-
 		j = 0;
 		x = i;
-
 		while (*(needle + j) != '\0')
 		{
 			if (*(haystack + x) == *(needle + j))
@@ -36,7 +37,6 @@ char *_strstr(char *haystack, char *needle)
 		{
 			return (needle);
 		}
-
 		i++;
 	}
 	return (0);
