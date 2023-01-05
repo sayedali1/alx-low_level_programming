6853b1d1c5a7b14d5d1c6d78b4565b00a9227853
sayed <said.ali199859@gmail.com>
2023-01-05 22:59:13 +0200

    task8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,37 +4,41 @@
 *check_astrc - cmp str when have astrc.
 *@s1: frist str
 *@s2: sec str
-*@holder: pointer to hold the str before
+*@holder: pointer to hold the str to use when we want to
+*check the char before 
 *Return: 1 if same, 0 otherwise
 */
+
 int check_astrc(char *s1, char *s2, char *holder)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')	/*str1 and str2 the same*/
 	{
 		return (1);
 	}
 
-	if (*s1 == '\0' && *s2 == '*')
+	if (*s1 == '\0' && *s2 == '*')	/*when the end of s1 and s2 current has astrc*/
 	{
 		return (check_astrc(s1, s2 + 1, s2 + 1));
 	}
 
-	if (*s1 == '\0' && *s2 != '\0')
+	if (*s1 == '\0' && *s2 != '\0')	/*when the end of s1 and not s2*/
 	{
 		return (0);
 	}
 
-	if (*s2 == '*')
+	if (*s2 == '*')	/*when s2 current char is astrc*/
 	{
 		return (check_astrc(s1, s2 + 1, s2 + 1));
 	}
 
-	if (*s1 == *s2)
+	if (*s1 == *s2)	/*when s1 the same of s2*/
 	{
+		/*go to the next char and the holder is the same*/
 		return (check_astrc(s1 + 1, s2 + 1, holder));
 	}
 	else
 	{
+		/*when not we go to the next char of s1 and s2 = the holder*/
 		return (check_astrc(s1 + 1, holder, holder));
 	}
 }
@@ -47,15 +51,15 @@ int check_astrc(char *s1, char *s2, char *holder)
 */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')	/*when s1 and s2 empty*/
 	{
 		return (1);
 	}
-	else if (*s1 == *s2)
+	else if (*s1 == *s2)	/*when str2 dont have astrc*/
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-	else if (*s2 == '*')
+	else if (*s2 == '*')	/*when we have astrc we use check_astrc*/
 	{
 		return (check_astrc(s1, s2 + 1, s2 + 1));
 	}
