0836f5d5d6e1a701e54acf44e332579126574004
sayed <said.ali199859@gmail.com>
2023-01-06 19:10:59 +0200

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc > 1)
+	if (argc >= 2)
 	{
 		mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 	}
 	else
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
