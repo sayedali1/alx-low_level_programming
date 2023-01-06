3638de9e0f309c932ff07d86257d2420714ba7c2
sayed <said.ali199859@gmail.com>
2023-01-06 18:27:34 +0200

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - pring the prog name
+*@argc: count of the argc elements
+*@argv: pointer to arg str
+*Return: 0
+*/
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
