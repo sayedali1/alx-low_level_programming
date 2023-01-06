53ea7bc224f59dff29b3bb1462cab320ee312b1e
sayed <said.ali199859@gmail.com>
2023-01-06 18:36:23 +0200

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - pring the prog name
+*@argc: count of the argc elements
+*@argv: pointer to arg str
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[0]);
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n",argv[i]);
+		}
+	}
+
+	return (0);
+}
