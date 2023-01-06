638f50cb484a8c0b755a8654c38a9a14bcbc5348
sayed <said.ali199859@gmail.com>
2023-01-06 18:55:31 +0200

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - mul two nums
+*@argc: count of the argc elements
+*@argv: pointer to arg str
+*Return: 1 if error ,0 otherwise
+*/
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc >= 1)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf ("%d\n", mul);
+	}
+	else 
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
