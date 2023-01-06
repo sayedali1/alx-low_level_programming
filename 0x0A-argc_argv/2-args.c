cf4cbda2759d878cb179c569ae4c5e19208769e6
sayed <said.ali199859@gmail.com>
2023-01-06 18:45:07 +0200

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,9 +10,11 @@ int main(int argc, char *argv[])
 {
 	int i;
 
+	printf("%s\n", argv[0]);
+
 	if (argc > 1)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 1; i < argc; i++)
 		{
 			printf("%s\n", argv[i]);
 		}
