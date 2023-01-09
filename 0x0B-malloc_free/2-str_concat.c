5755b1df28577f6880b6173b5439fe6ff4a0156c
sayed <said.ali199859@gmail.com>
2023-01-09 19:02:12 +0200

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,18 +37,38 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
+/**
+*str_concat - fun that concat two strings
+*@s1: pointer to str1
+*@s2: pointer to str2
+*Return: pointer to concatinated str, NULL otherwise
+*/
 char *str_concat(char *s1, char *s2)
 {
 	int len1, len2;
 	char *str;	/* str we concat strings in */
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
+	else if (s1 == NULL)
+	{
+		len1 = 0;
+		len2 = _strlen(s2);
+	}
+	else if (s2 == NULL)
+	{
+		len1 = _strlen(s1);
+		len2 = 0;
+	}
+	else
+	{
 	/* get the lenth of s1 and s2 */
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
+	}
+
 	/* create array size of s1 and s2 */
 	str = malloc((sizeof(char) * (len1 + len2)) + 1);
 
@@ -56,7 +76,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	
+
 	_strcpy(str, s1);/* cpy s1 into str */
 	_strcpy(&str[len1], s2);/* cpy s2 into str after s1 */
 	str[len1 + len2 + 1] = '\0';/* add null at the end of str */
