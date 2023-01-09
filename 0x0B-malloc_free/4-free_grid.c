4f4b354502742c43c64f7efdab4abc82dc9f3a17
sayed <said.ali199859@gmail.com>
2023-01-09 21:26:25 +0200

    task4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
 *free_grid - fun to free 2d memory alloc
-*@grid: is a 2d array we to free 
+*@grid: is a 2d array we to free
 *@height: is the num of colms
 *Rerurn: void
 */
