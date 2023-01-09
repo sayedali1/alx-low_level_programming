18548d0a53431ff2358794d8046846e0fe6562e1
sayed <said.ali199859@gmail.com>
2023-01-09 17:47:58 +0200

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strlen - get the len of the string
+* @s: the string
+* Return: the len
+*/
+int _strlen(char *s)
+{
+	int lenth = 0;
+
+	while (*s != '\0')
+	{
+		lenth++;
+		s++;
+	}
+
+	return (lenth);
+}
+/**
+*_strcpy - copy the sec str in the first one
+*@dest: the copied str
+*@src: the str we want to copy
+*Return: char the address of dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = _strlen(src);
+
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
+/**
+strdub - fun that cp a str into another
+*@str: pointer to str we want to duplicate
+*Return: pointer to the copied array
+*/
+
+char *_strdup(char *str)
+{
+	int len = _strlen(str);
+	/* create array of str size of str */    
+	char *cpstr = (char *)malloc((sizeof(char) * len) + 1);
+
+	if (cpstr == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	/* copy the sec str into cpstr */
+	_strcpy(cpstr, str);
+	return (cpstr);
+}
