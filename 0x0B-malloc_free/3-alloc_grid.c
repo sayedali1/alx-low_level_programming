773946b0e790f9a9546904ca53e8e12fbcf0f788
sayed <said.ali199859@gmail.com>
2023-01-09 20:23:19 +0200

    task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*alloc_grid - create two dimentional array
+*@width: the first dimention
+*@height: the sec dimention
+*Return: pointer to the first element of the array
+* 0 otherwise
+*/
+int **alloc_grid(int width, int height)
+{
+	int **array_2d;
+    int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	array_2d = malloc(sizeof(int) * height);/* allocate memory to rows */
+	if (array_2d == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < width; i++)
+	{
+		array_2d[i] = malloc(sizeof(int) * width);
+
+		if (array_2d[i] == NULL)
+		{
+			return (NULL);
+			free(array_2d[i]);
+			free(array_2d);
+		}		
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array_2d[i][j] = 0;
+		}
+	}
+	return (array_2d);
+}
