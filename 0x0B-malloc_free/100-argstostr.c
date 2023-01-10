5e869adfcbb7430a6154f800d84521d3c0c4f68e
sayed <said.ali199859@gmail.com>
2023-01-10 04:41:44 +0200

    task5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strlen - get the len of the string
+* @s: the string
+* Return: the len
+*/
+int _strlen(char *s)
+{
+	int lenth = 0;
+
+	while (*s != '\0')
+	{
+		lenth++;
+		s++;
+	}
+
+	return (lenth);
+}
+/**
+*argstostr - fun that concat all the argments in one str and split
+* with new line
+*@ac: numbers of argments
+*@av: argments that we want to splite */
+char *argstostr(int ac, char **av)
+{
+	int i, j, len, k = 0;
+	char *str;
+
+	if (ac == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1);
+
+	str = (char *)malloc(sizeof(char) * len);
+
+	if (str == NULL)
+	{
+		return(NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{ 
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			str[k++] = *(*(av + i) + j);
+		}
+		str[k++] = '\n';
+	}
+	str[k++] = '\0';
+
+	return (str);
+}
