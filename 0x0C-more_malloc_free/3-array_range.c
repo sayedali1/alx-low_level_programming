b8b627a1efd706ae105b56ec6ff9e565ff602845
sayed <said.ali199859@gmail.com>
2023-01-12 21:37:27 +0200

    task3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*array_range - fun that gets array for nums between the rangs
+*@min: min num
+*@max: max num
+*Return: pointer to the first element of the created array
+*NULL otherwise
+gi*/
+
+int *array_range(int min, int max)
+{
+	int *nums;
+	int i, j;
+
+	if (min < 0 || max < 0)
+	{
+		return (NULL);
+	}
+	if (min > max)
+	{
+		return (NULL);
+	}
+	nums = malloc(sizeof(max) * max - min);
+
+	for ( j = 0, i = min; i <= max; i++, j++)
+	{
+		nums[j] = i;
+
+	}
+	return (nums);
+}
