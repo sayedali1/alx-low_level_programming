3a00f22a503aa18511dd3d42b0fb4aeb89e7103b
sayed <said.ali199859@gmail.com>
2023-01-12 14:38:08 +0200

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -55,12 +55,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL )
 	{
-		s1 = "\0";
+		s1 = "";
 	}
 
 	if (s2 == NULL )
 	{
-		s2 = "\0";
+		s2 = "";
 	}
 
 	/* check if n greater or equal of s2 */
