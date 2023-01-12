b177e5cfe3a57b098adca50a906e9696b3de402d
sayed <said.ali199859@gmail.com>
2023-01-12 09:32:48 +0200

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*malloc_checked - fun that alloc memory of a given num
+*@b: num of memory we want to alloc
+*Return: void
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned *memory;
+
+	memory = malloc(sizeof *memory * b);
+
+	if (memory == NULL)
+	{
+		free(memory);
+		exit(98);
+	}
+	return(memory);
+}
