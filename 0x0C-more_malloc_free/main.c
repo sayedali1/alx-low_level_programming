5705feeca4069da61447a74edd94a16a5be8a3a8
sayed <said.ali199859@gmail.com>
2023-01-12 09:53:33 +0200

    main

--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -9,22 +9,22 @@
  */
 int main(void)
 {
-    char *c;
+   /*  char *c;
     int *i;
-    float *f;
+    float *f; */
     double *d;
 
-    c = malloc_checked(sizeof(char) * 1024);
+    /* c = malloc_checked(sizeof(char) * 1024);
     printf("%p\n", (void *)c);
     i = malloc_checked(sizeof(int) * 402);
     printf("%p\n", (void *)i);
     f = malloc_checked(sizeof(float) * 100000000);
-    printf("%p\n", (void *)f);
+    printf("%p\n", (void *)f); */
     d = malloc_checked(INT_MAX);
     printf("%p\n", (void *)d);
-    free(c);
+    /* free(c);
     free(i);
-    free(f);
+    free(f); */
     free(d);
     return (0);
 }
