c3f87fa200575d7a555584fd81bfdf409b5acf17
sayed <said.ali199859@gmail.com>
2023-01-12 13:30:23 +0200

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    int *memory;
+    unsigned int i;
+
+    if (nmemb == 0 || size == 0)
+    {
+        return (NULL);
+    }
+
+    memory = malloc(size * nmemb);
+    
+    if (memory == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < nmemb; i++)
+    {
+        memory[i] = 0;
+    }
+
+    return (memory);
+}
