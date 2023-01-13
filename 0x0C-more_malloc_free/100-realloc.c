f5b926e04afff75c4568d240a6b7e25891e02bf9
sayed <said.ali199859@gmail.com>
2023-01-13 02:07:54 +0200

    task4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*_realoc - fun that do the same as realloc fun
+*@ptr: pointer the the existing memory
+*@old_size: size of the old memory
+*@new_size: size of the new memory we want
+*Return: pointer to the new allocated memory, NULL otherwise
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *newPtr;
+	unsigned int i;
+
+	/* check for  new size */
+	if ((int)new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		new_size = new_size + old_size;
+		newPtr = malloc(new_size);
+		return (newPtr);
+	}
+
+	/* check the size to alloc memory  */
+	newPtr = malloc(new_size);
+
+	if (newPtr == NULL)
+	{
+		free(newPtr);
+		return (NULL);
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			newPtr[i] = *((char *)ptr +i);
+		}
+	}
+	else if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+		{
+			newPtr[i] = *((char *)ptr +i);
+		}
+	}
+	else
+	{
+		return (ptr);
+	}
+	free(ptr);
+	return (newPtr);
+}
