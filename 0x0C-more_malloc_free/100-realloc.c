a3c2e4acb3594ea24449bd59e38efaccd1bf8359
sayed <said.ali199859@gmail.com>
2023-01-13 02:18:53 +0200

    betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		new_size = new_size + old_size;
 		newPtr = malloc(new_size);
-		return (newPtr);
+
+		if ( newPtr == NULL)
+			return (NULL);
+		else
+			return (newPtr);
 	}
 	/* check the size to alloc memory  */
 	newPtr = malloc(new_size);
