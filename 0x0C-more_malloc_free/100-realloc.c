adba3cb5a2927d298a08ef75b19f0ed15d2ae5b7
sayed <said.ali199859@gmail.com>
2023-01-13 02:30:49 +0200

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	/* check for  new size */
-	if ((int)new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
