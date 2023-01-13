0bcbc5c6d05628b7f192d85f64fad3ca9a94625f
sayed <said.ali199859@gmail.com>
2023-01-13 02:23:24 +0200

    betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		new_size = new_size + old_size;
 		newPtr = malloc(new_size);
 
 		if ( newPtr == NULL)
