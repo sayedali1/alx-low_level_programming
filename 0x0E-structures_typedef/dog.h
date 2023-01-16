cfc3fa61ab5d6ceecd08a760d164079a7254fb99
sayed <said.ali199859@gmail.com>
2023-01-16 12:17:25 +0200

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,11 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+void print_dog(struct dog *d);
+
+/**
+*dog_t - typedef struct dog
+*/
+typedef struct dog dog_t;
+
 #endif
