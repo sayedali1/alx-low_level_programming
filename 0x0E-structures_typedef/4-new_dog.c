900c3b2d9bdfdb411609e4a88c8140e367cd9774
sayed <said.ali199859@gmail.com>
2023-01-16 13:43:45 +0200

    comments

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,7 +53,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *owner_cp;
 
 	New_dog = malloc(sizeof(dog_t));/* allocate memory of size dog_t */
-
 	if (New_dog == NULL)
 		return (NULL);
 
@@ -75,7 +74,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		/* alloc memory of size owner and cpy owner into owner_cp */
 		owner_cp = malloc(sizeof(char) * _strlen(owner) + 1);
-
 		if (owner_cp == NULL)
 		{
 			free(name_cp);
