dd599ae4f4823e9e0d4813fcf680f8d6f3066622
sayed <said.ali199859@gmail.com>
2023-01-18 16:19:14 +0200

    task3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - program that performs simple operations
+*@ac: num of arguments
+*@av: array of arguments
+*Return: 0
+*/
+
+int main (int ac , char **av)
+{
+    int (*get_fun)(int, int);
+
+    if (ac <= 1)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    if (get_op_func(av[2]) == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    if ((av[2][0] == '/' || av[2][0] == '%' ) && atoi(av[3]) == 0)
+    {
+        printf("Error");
+        exit(100);
+    }
+
+    get_fun = get_op_func(av[2]);
+    printf("%d\n", get_fun(atoi(av[1]), atoi(av[3])));
+
+    return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+
+/**
+*op_add - fun that add two nums
+*@a:first num
+*@b:sec num
+*Return: int
+*/
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+*op_sub - fun that sub two nums
+*@a:first num
+*@b:sec num
+*Return: int
+*/
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+*op_mul - fun that mul two nums
+*@a:first num
+*@b:sec num
+*Return: int
+*/
+
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+*op_div - fun that div two nums
+*@a:first num
+*@b:sec num
+*Return: int
+*/
+
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+*op_mod - fun that mod two nums
+*@a:first num
+*@b:sec num
+*Return: int
+*/
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
