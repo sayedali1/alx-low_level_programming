d1c4efd04bbca9615b494412e61551670b74d37c
sayed <said.ali199859@gmail.com>
2023-01-18 16:20:29 +0200

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,28 +11,28 @@
 
 int main (int ac , char **av)
 {
-    int (*get_fun)(int, int);
+	int (*get_fun)(int, int);
 
-    if (ac <= 1)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (ac <= 1)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    if (get_op_func(av[2]) == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (get_op_func(av[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((av[2][0] == '/' || av[2][0] == '%' ) && atoi(av[3]) == 0)
-    {
-        printf("Error");
-        exit(100);
-    }
+	if ((av[2][0] == '/' || av[2][0] == '%' ) && atoi(av[3]) == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
 
-    get_fun = get_op_func(av[2]);
-    printf("%d\n", get_fun(atoi(av[1]), atoi(av[3])));
+	get_fun = get_op_func(av[2]);
+	printf("%d\n", get_fun(atoi(av[1]), atoi(av[3])));
 
-    return (0);
+	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,7 @@
 */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
@@ -19,7 +19,7 @@ int op_add(int a, int b)
 */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
 /**
@@ -31,7 +31,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 /**
@@ -43,7 +43,7 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 
 /**
@@ -54,5 +54,5 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
