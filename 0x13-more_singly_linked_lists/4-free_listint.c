e8ae7796a2cc33d7db59621e66b2e35bc5fd06b8
sayed <said.ali199859@gmail.com>
2023-01-30 18:18:48 +0200

    task4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,10 +7,13 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
+	if (head == NULL)
+		return;
+
 	while (head != NULL)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = head;
+		head = temp->next;
+		free(temp);
 	}
 }
