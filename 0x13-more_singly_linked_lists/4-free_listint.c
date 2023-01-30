299761643c9a9f53e35c0c607293adbda7f5f7c6
sayed <said.ali199859@gmail.com>
2023-01-30 18:19:59 +0200

    betty

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,13 +7,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
-		return;
-
 	while (head != NULL)
 	{
-		temp = head;
-		head = temp->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
