a501d28cc293f2b439312f51e608522c610e5913
sayed <said.ali199859@gmail.com>
2023-01-30 18:37:58 +0200

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+    listint_t *temp;
+
+    if (head == NULL)
+        return;
+
+    while (temp->next != NULL)
+    {
+        temp = *head;
+        *head = temp->next;
+        free(temp);
+    }
+    head = NULL;
+}
