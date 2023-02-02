a09f0acbd4f3044e047a8fa56a26356b8341cd6f
sayedali1 <said.ali199859@gmail.com>
2023-02-02 19:32:30 +0200

    task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,30 +8,30 @@ void print_binary(unsigned long int n)
 	int i, flag = 0;
 	unsigned long int  decimal = 1024, num = 0;
 
-	if (n < 0)
-		return;
-
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
 	/* walk through 1024 */
-	for (i = 0; i < 11; i++)
+	if (n > 0)
 	{
-		/* check if num plus the decimal less than the num */
-		if (n >= (num + decimal))
+		for (i = 0; i < 11; i++)
 		{
-			_putchar('1');/* print 1 */
-			num += decimal; /* add decimal to num */
-			decimal >>= 1; /* devide the decimal by 2 */
-			flag = 1; /* use it to dont print 0 on the left */
-		}
-		else if (n < (num + decimal))
-		{
-			if (flag == 1)
-				_putchar('0'); /* print 0 only if 1 printed before it */
-			decimal >>= 1; /* devide decimal by 2 */
+			/* check if num plus the decimal less than the num */
+			if (n >= (num + decimal))
+			{
+				_putchar('1');/* print 1 */
+				num += decimal; /* add decimal to num */
+				decimal >>= 1; /* devide the decimal by 2 */
+				flag = 1; /* use it to dont print 0 on the left */
+			}
+			else if (n < (num + decimal))
+			{
+				if (flag == 1)
+					_putchar('0'); /* print 0 only if 1 printed before it */
+				decimal >>= 1; /* devide decimal by 2 */
+			}
 		}
 	}
 }
