831ee8f21070a36725063228662af0f03cbbcc00
sayedali1 <said.ali199859@gmail.com>
2023-02-02 19:23:43 +0200

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_binary - fun that convert decimal to binary
+ * @n:num we want to convert
+ */
+void print_binary(unsigned long int n)
+{
+	int i, flag = 0;
+	unsigned long int  decimal = 1024, num = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	/* walk through 1024 */
+	for (i = 0; i < 11; i++)
+	{
+		/* check if num plus the decimal less than the num */
+		if (n >= (num + decimal))
+		{
+			_putchar('1');/* print 1 */
+			num += decimal; /* add decimal to num */
+			decimal >>= 1; /* devide the decimal by 2 */
+			flag = 1; /* use it to dont print 0 on the left */
+		}
+		else if (n < (num + decimal))
+		{
+			if (flag == 1)
+				_putchar('0'); /* print 0 only if 1 printed before it */
+			decimal >>= 1; /* devide decimal by 2 */
+		}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,11 @@
 #ifndef main_h
 #define main_h
-
-
+#include <stdio.h>
 unsigned int binary_to_uint(const char *b);
+
+void print_binary(unsigned long int n);
+
 int _putchar(char c);
+
 
 #endif
