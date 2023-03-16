646dfb81cd63d9514ca60aac873fdf4f4d46e3ea
sayedali1 <said.ali199859@gmail.com>
2023-03-16 06:37:41 +0200

    task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - fun that get the len of a linked list
+ * @h: pointer the the first element in the list
+ * Return: the len of the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *temp = h;
+    int i;
+
+    if (h == NULL)
+        return (0);
+
+    for (i = 0; temp; i++)
+        temp = temp->next;
+    return (i);
+}
