b90f7c3b4581a326bc941af109759e231e8d0654
sayedali1 <said.ali199859@gmail.com>
2023-03-16 06:36:53 +0200

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - fun that print a linked list
+ * @h: pointer to the first element in the list
+ * Return: size of the linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	int i;
+	if (h = NULL)
+		return (0);
+
+	for (i = 0; temp; i++)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+
+	return (i);
+}
