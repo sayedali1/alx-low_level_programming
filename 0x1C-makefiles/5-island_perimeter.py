b975a8a530bd7261096ba044013c0c83071b5379
sayedali1 <said.ali99859@gmail.com>
2023-07-26 14:42:53 +0300

    task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,22 +7,15 @@ Return the perimeter of a given matrix
 
 def island_perimeter(grid):
     """ fun that calc the perimeter of a given matrix """
-    perimeter = 0
+    islands = 0
     if len(grid) <= 100 and len(grid[0]) <= 100:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    perimeter += 4
-                    try:
-                        if grid[i - 1][j] == 1:
-                            perimeter -= 1
-                        if grid[i + 1][j] == 1:
-                            perimeter -= 1
-                        if grid[i][j - 1] == 1:
-                            perimeter -= 1
-                        if grid[i][j + 1] == 1:
-                            perimeter -= 1
-                    except IndexError:
-                        pass
+                     islands += 1
+  
+    if islands == 0:
+        return 0
+    else:
+        return  int(((4 * islands) / 2) + 2) 
 
-    return perimeter
