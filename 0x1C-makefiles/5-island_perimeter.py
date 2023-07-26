a1c3e27835d0fb786025a3b318e6ce1f1f06dc77
sayedali1 <said.ali99859@gmail.com>
2023-07-26 15:04:14 +0300

    task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Modul: island_perimeter
 Return the perimeter of a given matrix
 """
@@ -12,10 +12,11 @@ def island_perimeter(grid):
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
-                     islands += 1
-  
+                    islands += 1
+
     if islands == 0:
         return 0
+    elif islands % 2 == 0:
+        return int(((4 * islands) / 2) + 2)
     else:
-        return  int(((4 * islands) / 2) + 2) 
-
+        return int(((4 * islands) / 2))
